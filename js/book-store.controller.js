efcd57e4b6ec1bd0e1e464e0bba9d21bdc7fd856
UriShilo <urishilo360@gmail.com>
2023-11-19 20:13:04 +0200

    Implement addBook() & removeBook()

--- js/book-store.controller.js
+++ js/book-store.controller.js
@@ -2,41 +2,51 @@
 
 
 
-function onInit(){
+function onInit() {
   // getBooks()
   renderBooks()
-  console.log(gBooks)
+  // console.log(gBooks)
 
 }
 
-function renderBooks(){
-  const strHTML = getBooks().map((book)=>{
-    console.log(book)
+function renderBooks() {
+  const strHTML = getBooks().map((book) => {
+    // console.log(book)
     return `<tr>
     <td>${book.id}</td>
     <td>${book.title}</td>
     <td>$${book.price}</td>
-    <td><button onclick="onRead(${book.id})">Read</button></td>
-    <td><button onclick="onUpdate()">Update</button></td>
-    <td><button onclick="onDelete()">Delete</button></td>
+    <td><button onclick="onRead('${book.id}')">Read</button></td>
+    <td><button onclick="onUpdate('${book.id}')">Update</button></td>
+    <td><button onclick="onRemoveBook('${book.id}')">Delete</button></td>
     </tr>`
   }).join('')
 
   const tbody = document.querySelector('tbody')
-  tbody.innerHTML=strHTML
+  tbody.innerHTML = strHTML
 }
 
-function onReadBook(){
-// onReadBook(bookId)
-// strHTML = <modal elements go here...>
-// <h2>Book name<h/h2>
-// <modal>
+function onAddBook() {
+  var title = prompt('book title:')
+  var price = prompt('book price:')
+
+if(!title || !price)return
+
+  addBook(title, price)
+  renderBooks()
+}
+
+function onReadBook() {
+  // onReadBook(bookId)
+  // strHTML = <modal elements go here...>
+  // <h2>Book name<h/h2>
+  // <modal>
 
 }
-function onUpdateBook(){
+function onUpdateBook() {
   // updateRate(bookId, bookPrice)
 }
-function onRemoveBook(){
-// removeBook(bookId)
-// renderBooks()
+function onRemoveBook(bookId) {
+  removeBook(bookId)
+  renderBooks()
 }
--- js/services/book-store.service.js
+++ js/services/book-store.service.js
@@ -1,26 +1,58 @@
 'use strict'
 
+const STORAGE_KEY = 'bookDB'
+
 var gBooks
 
+_createBooks()
+
 function getBooks() {
-  gBooks = [
-    _createBook('harry potter', 16),
-    _createBook('lord of the rings', 23),
-    _createBook('snow crush', 8)
-  ]
   return gBooks
 }
 
 
 
+function addBook(title, price) {
+  console.log(title, price)
+  const book = _createBook(title, price)
+  gBooks.push(book)
+  saveToStorage(STORAGE_KEY, gBooks)
+
+  return book
+
+}
+
 function readBook() {
 
 }
+
 function updateBook() {
 
 }
-function removeBook() {
 
+function removeBook(bookId) {
+  const bookIdx = gBooks.findIndex((book) => { book.id === bookId })
+  const book = gBooks.splice(bookIdx, 1)
+  saveToStorage(STORAGE_KEY, gBooks)
+  return book
+}
+
+
+
+
+function _createBooks() {
+  gBooks = loadFromStorage(STORAGE_KEY)
+  if (gBooks && gBooks.length) return
+
+  // If no books in storage - generate demo data
+
+  gBooks = [
+    _createBook('Harry Potter', 16),
+    _createBook('Lord of the Rings', 23),
+    _createBook('Snow Crush', 8)
+  ]
+
+  saveToStorage(STORAGE_KEY, gBooks)
 }
 
 function _createBook(title, price) {
